ae2a0968a18d69d3459aa1741b706e8dcf97c67d
Yami <dlplima@hotmail.com>
2022-07-13 22:08:13 -0300

    main

--- rispar_simulator_provider/lib/main.dart
+++ rispar_simulator_provider/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:device_preview/device_preview.dart';
+//import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app/app_module.dart';
@@ -9,6 +9,6 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  //runApp(const AppModule());
-  runApp(DevicePreview(builder: (_) => const AppModule()));
+  runApp(const AppModule());
+  //runApp(DevicePreview(builder: (_) => const AppModule()));
 }
